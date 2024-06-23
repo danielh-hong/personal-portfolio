6fce5334cabe885f24b3efb3ac018ecad7204564
danielh-hong <danielh.toronto@gmail.com>
2024-06-23 13:37:41 -0400

    Attempted to improve responsiveness to 4k. At least changed the width. If stuff looks messed up, revert to push b efore this.

--- assets/js/script.js
+++ assets/js/script.js
@@ -141,11 +141,11 @@ for (let i = 0; i < formInputs.length; i++) {
   });
 }
 
+const navigationLinks = document.querySelectorAll("[data-nav-link]");
 
 
 document.addEventListener("DOMContentLoaded", function() {
   // page navigation variables
-  const navigationLinks = document.querySelectorAll("[data-nav-link]");
   const pages = document.querySelectorAll("[data-page]");
 
   // function to update active page and nav link
